75b34d095008e4e95c5ef039bf5b9a942d7d17da
kamal-prog-code <karrerajisyam123@gmail.com>
2021-08-18 10:27:59 +0530

    Install,gst,cgst

--- blank_django/settings.py
+++ blank_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0a3e*wdy)lqg=x_$4l#ni3!n=!d5+fp1vkl+4m89jr0f*sl@is'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["hkrsinvgen.herokuapp.com","127.0.0.1"]
+ALLOWED_HOSTS = ["hkrs-inv.herokuapp.com","127.0.0.1"]
 
 
 CORS_ALLOW_ALL_ORIGINS = True
